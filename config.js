cf5bc9fb22744242997562b02dd71ab11a6fcd56
aarti <artikabra001@gmail.com>
2023-10-09 14:48:40 +0530

    before merge

---
+++ config.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://artikabra001:1234567890@atlascluster.ycv2xua.mongodb.net/e-comm?retryWrites=true&w=majority")
+
---
+++ product.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name:String,
+    price:Number
+});
+module.exports=mongoose.model('products',productSchema)
